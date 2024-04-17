5e296effba11a9056391576015c936528c0c3f07
Çağrı <karpuzcagri@gmail.com>
2024-04-17 23:07:59 +0900

    Update list_list.py

--- Python List exercise/list_list.py
+++ Python List exercise/list_list.py
@@ -2,3 +2,8 @@ letter = ["a", "b", "c", "d"]
 matrix = [[1, 2], [2, 2], [3, 9]]
 zeros = [0] * 100
 print(zeros)
+
+numbers = list(range(20))
+print(numbers)
+char = list("Hello World")
+print(len(char))
